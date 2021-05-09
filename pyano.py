cc60b4fb15816aeb00bec836ce153924427e2f87
Khoan Tang Mong <khoanmtk@gmail.com>
2021-05-09 23:44:24 +0700

    add pedal interval

--- pyano.py
+++ pyano.py
@@ -3,6 +3,9 @@ import configparser
 from mido import MidiFile, tempo2bpm
 from datetime import datetime
 
+from mido.messages.messages import Message
+from mido.midifiles.tracks import MidiTrack
+
 class Pyano:
     def __init__(self, midi_file, file_name = 'file', min=70, max=110, range=10):
         self.midi_file = midi_file
@@ -32,8 +35,31 @@ class Pyano:
                 self.output_text += str(msg) + "\n"
     
     # Add pedal to the midi file
-    def add_pedal(self):
-        pass
+    def add_pedal_interval(self, interval):
+        # calculate time pass in midi
+        time_pass = 0
+        for i, track in enumerate(midi_file.tracks):
+            if time_pass != 0:
+                for msg in track:
+                    if msg.type == "note_on" or msg.type == "note_off" \
+                            or msg.type == "control_change":
+                        time_pass += msg.time
+
+        # Add pedal with interval to new track.
+        pedal_track = MidiTrack()
+        time_pass_pedal = 0
+        while time_pass_pedal < time_pass:
+            # Add pedal off and pedal all before and after this note
+            pedal_msg_on = Message("control_change", 
+                                    channel = 2, control = 64, 
+                                    value = 100, time = 1)
+            pedal_msg_off = Message("control_change", 
+                                    channel = 2, control = 64, 
+                                    value = 0, time = interval - 1)
+            pedal_track.append(pedal_msg_on)
+            pedal_track.append(pedal_msg_off)
+            time_pass_pedal += interval
+        self.midi_file.tracks.append(pedal_track)
 
     # simplify the left hand to only press the chord
     def simplify_left_hand(self):
@@ -43,17 +69,18 @@ class Pyano:
     def write_output_midi(self, output_folder):
         if not os.path.isdir(output_folder):
             os.mkdir(output_folder)
-        file_output = output_folder + 'enhanced_' + self.file_name
+        file_output = output_folder + "enhanced_" + self.file_name
+        self.midi_file.save(file_output)
 
-        f = open(file_output,"w")
-        f.write(self.output_text)
-        f.close()
     
     def write_output_text(self, output_folder):
         if not os.path.isdir(output_folder):
             os.mkdir(output_folder)
-        file_output = output_folder + self.file_name.replace('.mid', '.txt')
-        self.midi_file.save(file_output)
+        file_output = output_folder + self.file_name.replace(".mid", ".txt")
+
+        f = open(file_output,"w")
+        f.write(self.output_text)
+        f.close()
 
 # main call
 if __name__ == "__main__":
@@ -65,15 +92,13 @@ if __name__ == "__main__":
     min = config['threshold']['min']
     max = config['threshold']['max']
     random_range = config['threshold']['random-range']
+    pedal_interval = config['pedal']['interval']
     to_write_midi = False
 
     pyano = Pyano(midi_file, min, max, random_range)
-
-    if config['feature'].getboolean('midi-to-text'):
-        pyano.midi_to_text()
-        pyano.write_output_text(output_folder)
     
     if config['feature'].getboolean('add-pedal'):
+        pyano.add_pedal_interval(pedal_interval)
         to_write_midi = True
 
     if config['feature'].getboolean('simplify-left-hand'):
@@ -82,6 +107,10 @@ if __name__ == "__main__":
     if config['feature'].getboolean('apply-threshold'):
         to_write_midi = True
         pyano.correct_velocity()
+
+    if config['feature'].getboolean('midi-to-text'):
+        pyano.midi_to_text()
+        pyano.write_output_text(output_folder)
     
     if to_write_midi:
         pyano.write_output_midi(output_folder)
