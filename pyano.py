02da5cbca2dd2830625ee47b6aab34977e7435bf
Khoan Tang Mong <khoanmtk@gmail.com>
2021-05-11 01:30:17 +0700

    correct velocity and add pedal

--- pyano.py
+++ pyano.py
@@ -1,9 +1,11 @@
 import os
 import configparser
-from mido import MidiFile, tempo2bpm
+import random
+from mido import MidiFile
 from datetime import datetime
 
 from mido.messages.messages import Message
+from mido.midifiles.meta import MetaMessage
 from mido.midifiles.tracks import MidiTrack
 
 class Pyano:
@@ -14,51 +16,62 @@ class Pyano:
         self.max = max
         self.range = range
         self.output_text = ""
-        #midi_file = MidiFile(file_name
 
     # Correct velocity function
     def correct_velocity(self):
         for i, track in enumerate(midi_file.tracks):
             for msg in track:
                 if msg.type == "note_on":
-                    msg.velocity = 100
+                    msg.velocity = int(msg.velocity + self.range * random.uniform(-1,1))
+                    if msg.velocity > self.max:
+                        msg.velocity = self.max
+                    if msg.velocity < self.min:
+                        msg.velocity = self.min
 
     # Midi to text function
     def midi_to_text(self):
         # Get path
-        midi_file = MidiFile(input_file)
-
-        for i,track in enumerate(midi_file.tracks):
+        for i,track in enumerate(self.midi_file.tracks):
             self.output_text += f"Track {i}: {track.name}\n"
             # print(f"Track {i}: {track.name}\n")
             for msg in track:
                 self.output_text += str(msg) + "\n"
     
     # Add pedal to the midi file
-    def add_pedal_interval(self, interval):
+    def add_pedal(self):
         # calculate time pass in midi
         time_pass = 0
-        for i, track in enumerate(midi_file.tracks):
-            if time_pass != 0:
+        beat_per_bar = 0
+        pedal_track = MidiTrack()
+        pedal_track.name = "pedal"
+
+        # Trace the midi to find the information of midi length and beat
+        for track in self.midi_file.tracks:
+            if time_pass == 0:
                 for msg in track:
+                    if msg.type == "time_signature":
+                        beat_per_bar = msg.numerator
                     if msg.type == "note_on" or msg.type == "note_off" \
                             or msg.type == "control_change":
                         time_pass += msg.time
-
+        
         # Add pedal with interval to new track.
-        pedal_track = MidiTrack()
         time_pass_pedal = 0
+        interval = self.midi_file.ticks_per_beat * beat_per_bar
         while time_pass_pedal < time_pass:
             # Add pedal off and pedal all before and after this note
+            pedal_msg_off = Message("control_change", 
+                                    channel = 2, control = 64, 
+                                    value = 0, time = interval-1)
             pedal_msg_on = Message("control_change", 
                                     channel = 2, control = 64, 
                                     value = 100, time = 1)
-            pedal_msg_off = Message("control_change", 
-                                    channel = 2, control = 64, 
-                                    value = 0, time = interval - 1)
-            pedal_track.append(pedal_msg_on)
+
             pedal_track.append(pedal_msg_off)
+            pedal_track.append(pedal_msg_on)
             time_pass_pedal += interval
+        eot = MetaMessage('end_of_track', time=0)
+        pedal_track.append(eot)
         self.midi_file.tracks.append(pedal_track)
 
     # simplify the left hand to only press the chord
@@ -89,16 +102,15 @@ if __name__ == "__main__":
     input_file = config['file']['input-file']
     output_folder = config['file']['output-folder']
     midi_file = MidiFile(input_file)
-    min = config['threshold']['min']
-    max = config['threshold']['max']
-    random_range = config['threshold']['random-range']
-    pedal_interval = config['pedal']['interval']
+    min = int(config['threshold']['min'])
+    max = int(config['threshold']['max'])
+    random_range = int(config['threshold']['random-range'])
     to_write_midi = False
 
     pyano = Pyano(midi_file, min, max, random_range)
     
     if config['feature'].getboolean('add-pedal'):
-        pyano.add_pedal_interval(pedal_interval)
+        pyano.add_pedal()
         to_write_midi = True
 
     if config['feature'].getboolean('simplify-left-hand'):
@@ -114,4 +126,3 @@ if __name__ == "__main__":
     
     if to_write_midi:
         pyano.write_output_midi(output_folder)
-    
